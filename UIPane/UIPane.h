273b81eccc349fa774274731fc0fc84dcc452879
andrzejsemeniuk <andrzejsemeniuk@gmail.com>
2017-07-14 06:30:44 -0700

    Initial Commit

---
+++ UIPane/UIPane.h
@@ -0,0 +1,19 @@
+//
+//  UIPane.h
+//  UIPane
+//
+//  Created by andrzej semeniuk on 7/14/17.
+//  Copyright © 2017 Andrzej Semeniuk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for UIPane.
+FOUNDATION_EXPORT double UIPaneVersionNumber;
+
+//! Project version string for UIPane.
+FOUNDATION_EXPORT const unsigned char UIPaneVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIPane/PublicHeader.h>
+
+
